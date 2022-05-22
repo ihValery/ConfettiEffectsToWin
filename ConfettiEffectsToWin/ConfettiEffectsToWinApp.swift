bd082568d5daeb1acd3fe82800d8b1432b09d79f
ihValery <ihValery@gmail.com>
2022-05-22 16:38:45 +0300

    Initial Commit

---
+++ ConfettiEffectsToWin/ConfettiEffectsToWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiEffectsToWinApp.swift
+//  ConfettiEffectsToWin
+//
+//  Created by Валерий Игнатьев on 22.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiEffectsToWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
